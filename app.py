ad17ae32d26e395911e26c33ae3dc9ad36173b44
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2023-10-31 22:31:50 +0800

    Update app.py
    - unimported utils and numpy
    - erase some face points
    - euclaideanDistance return
    - erased related to fps
    - UI related deleted

--- app.py
+++ app.py
@@ -2,24 +2,22 @@
 import cv2 as cv
 import mediapipe as mp
 import time
-import utils, math
-import numpy as np
+import math
+
 # variables 
 frame_counter =0
 CEF_COUNTER =0
 TOTAL_BLINKS =0
 
+eopen_time = 0
+eclose_time = 0
+
+
 # constants
 CLOSED_EYES_FRAME =3
 FONTS =cv.FONT_HERSHEY_COMPLEX
+blink_above_avg = 0.4
 
-# face bounder indices 
-# FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
-
-# lips indices for Landmarks
-# LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
-# LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
-# UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
 
 # Left eyes indices 
 LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
@@ -47,8 +45,7 @@ def landmarksDetection(img, results, draw=False):
 def euclaideanDistance(point, point1):
     x, y = point
     x1, y1 = point1
-    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
-    return distance
+    return math.sqrt((x1 - x)**2 + (y1 - y)**2)
 
 # Blinking Ratio
 def blinkRatio(img, landmarks, right_indices, left_indices):
@@ -83,19 +80,11 @@ def blinkRatio(img, landmarks, right_indices, left_indices):
 
 
 
-with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh:
-
-    # starting time here 
-    start_time = time.time()
-    eopen_time = 0
-    eclose_time = 0
-
-    # deletable variables
-    last_close_sec = 0
+with map_face_mesh.FaceMesh(min_detection_confidence =0.9, min_tracking_confidence=0.9) as face_mesh:
 
     # starting Video loop here.
     while True:
-        frame_counter +=1 # frame counter
+        
         ret, frame = camera.read() # getting frame from camera 
         if not ret: 
             break # no more frames break
@@ -111,17 +100,14 @@ with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confiden
         if results.multi_face_landmarks:
             mesh_coords = landmarksDetection(frame, results)
             ratio = blinkRatio(frame, mesh_coords, RIGHT_EYE, LEFT_EYE)
-            # cv.putText(frame, f'ratio {ratio}', (100, 100), FONTS, 1.0, utils.GREEN, 2)
-            utils.colorBackgroundText(frame,  f'Ratio : {round(ratio,2)}', FONTS, 0.7, (30,100),2, utils.PINK, utils.YELLOW)
             
-            if ratio >5:
+            if ratio >5.5:
                 CEF_COUNTER +=1
                 eclose_time = time.time() - eopen_time
-                last_close_sec = eclose_time
-                # cv.putText(frame, 'Blink', (200, 50), FONTS, 1.3, utils.PINK, 2)
-                # utils.colorBackgroundText(frame,  f'Blink', FONTS, 1.7, (int(frame_height/2), 100), 2, utils.YELLOW, pad_x=6, pad_y=6, )
                 
                 print(eclose_time)
+                if eclose_time > blink_above_avg:
+                    print("Stay Awake")
             else:
                 eopen_time = time.time()
                 eclose_time = 0
@@ -130,27 +116,13 @@ with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confiden
                     TOTAL_BLINKS += 1
                     CEF_COUNTER = 0
                 
-                if eclose_time > 2.5:
-                    utils.colorBackgroundText(frame,  f'AYAWG KATOG!!', FONTS, 1.7, (int(frame_height/2), 200), 2, utils.YELLOW, pad_x=6, pad_y=6, )
-
-            # cv.putText(frame, f'Total Blinks: {TOTAL_BLINKS}', (100, 150), FONTS, 0.6, utils.GREEN, 2)
-            utils.colorBackgroundText(frame,  f'Total Blinks: {TOTAL_BLINKS}', FONTS, 0.7, (30,150),2)
-            utils.colorBackgroundText(frame,  f'Eye Open(sec): {eopen_time}', FONTS, 0.7, (30,200),2)
-            utils.colorBackgroundText(frame,  f'Eye Close(sec): {eclose_time}', FONTS, 0.7, (30,250),2)
-            utils.colorBackgroundText(frame,  f'Eye Close(sec): {last_close_sec}', FONTS, 0.7, (30,300),2)
-            cv.polylines(frame,  [np.array([mesh_coords[p] for p in LEFT_EYE ], dtype=np.int32)], True, utils.GREEN, 1, cv.LINE_AA)
-            cv.polylines(frame,  [np.array([mesh_coords[p] for p in RIGHT_EYE ], dtype=np.int32)], True, utils.GREEN, 1, cv.LINE_AA)
+               
         
         # calculating  frame per seconds FPS
-        end_time = time.time()-start_time
-        fps = frame_counter/end_time
 
-        frame =utils.textWithBackground(frame,f'FPS: {round(fps,1)}',FONTS, 1.0, (30, 50), bgOpacity=0.9, textThickness=2)
-        # writing image for thumbnail drawing shape
-        # cv.imwrite(f'img/frame_{frame_counter}.png', frame)
-        cv.imshow('cammera', frame)
-        key = cv.waitKey(2)
-        if key==ord('q') or key ==ord('Q'):
-            break
+        # frame =utils.textWithBackground(frame,f'FPS: {round(fps,1)}',FONTS, 1.0, (30, 50), bgOpacity=0.9, textThickness=2)
+        # key = cv.waitKey(2)
+        # if key==ord('q') or key ==ord('Q'):
+        #     break
     cv.destroyAllWindows()
     camera.release()
