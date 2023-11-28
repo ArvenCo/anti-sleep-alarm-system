4e8d29c9673f64dff29a64275ef9d1d892765fc5
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2023-11-28 22:32:32 +0800

    insert user if none

--- server/controller/users.py
+++ server/controller/users.py
@@ -5,8 +5,11 @@ def index():
     return user
 
 def insert(username:str):
-    user = User(username=username)
-    db.session.add(user)
-    db.session.commit()
+    user = User.query.filter(User.username == username).first()
+    if user is None:
+        new_user = User(username=username)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
     return user
 
--- server/routes/socket.py
+++ server/routes/socket.py
@@ -1,23 +1,26 @@
 from imports import *
 from server.controller.system import *
+import server.controller.users as UserController
+import server.controller.events as EventController
+
 socketio = SocketIO(cors_allowed_origins="*")
 
 
 
-    
 
 @socketio.on('camera_stream')
 def camera_stream(json):
 
-    
     matt_img= base64_to_matt(json['frame'])
     data = process(matt_img)
+    user = UserController.insert(json['user'])
     
     data.update({
         'img': matt_to_base64(matt_img)
     })
-
+    
     if float(data['eye']) > 5:
+        
         emit('notif-eye', data['eye'], broadcast=True) 
 
     if float(data['mouth']) < 1:
